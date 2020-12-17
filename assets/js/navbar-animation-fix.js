785c3841cca45f09b727b5c95d2c477598355894
Beatriz S Medina <bee.a2@hotmail.com>
2020-12-17 14:43:42 -0300

    Resolvendo bug do menu com o banner

--- assets/js/navbar-animation-fix.js
+++ assets/js/navbar-animation-fix.js
@@ -6,3 +6,5 @@ $('#collapse-navbar').on('show.bs.collapse', function() {
 $('#collapse-navbar').on('hide.bs.collapse', function() {
 	$('.topCasaFina-banner').css('transform', 'translate(-50%, -50%');
 });
+
+//Faz o banner subir/descer conforme o menu hamburguer aparece ou esconde;
